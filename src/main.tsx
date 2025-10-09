f5b69ea741a375108a3dae73c331e86276713143
Pruuingoo <168852932+Pruuingoo@users.noreply.github.com>
2025-10-09 16:42:52 +0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-import { inject } from '@vercel/speed-insights';
-
+// Import and inject Vercel Speed Insights
+import { inject } from "@vercel/speed-insights";
 inject();
 
 createRoot(document.getElementById("root")!).render(<App />);
