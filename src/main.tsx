2f8dc9b7d51f3aa4a98dc00d3bcf64f591340d50
Pruuingoo <168852932+Pruuingoo@users.noreply.github.com>
2025-10-09 16:47:13 +0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-// Import and inject Vercel Speed Insights
-// Use `// @ts-ignore` because TypeScript has no types for this package
-// This tells TypeScript to ignore the next line
-// It will still work perfectly at runtime
-// Make sure the package is installed in package.json
-// npm install @vercel/speed-insights
-// or add manually in GitHub dependencies
-
-// @ts-ignore
 import { inject } from "@vercel/speed-insights";
 inject();
 
